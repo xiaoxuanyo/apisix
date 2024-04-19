dd324fad0acb66c416dcbef5cc6150eecc3670b8
14719 <xiaoxuanemail@163.com>
2024-04-20 03:20:05 +0800

    build-apisix-runtime.sh 1.2.1配置

--- utils/install-dependencies.sh
+++ utils/install-dependencies.sh
@@ -123,10 +123,9 @@ function multi_distro_uninstallation() {
 
 function install_apisix_runtime() {
     export runtime_version=${APISIX_RUNTIME:?}
-    pwd
     # wget "https://raw.githubusercontent.com/api7/apisix-build-tools/apisix-runtime/${APISIX_RUNTIME}/build-apisix-runtime.sh"
     chmod +x ../build-apisix-runtime.sh
-    ../build-apisix-runtime.sh latest
+    ./build-apisix-runtime.sh latest
     # rm build-apisix-runtime.sh
 }
 
